836c647f050ac8ee5085dc4c13887ab626c88846
Anttu <78537438@qq.com>
2020-12-26 20:56:09 +0800

    打包增加ico图标方式调整用rsrc

--- md5/cmd.bat
+++ md5/cmd.bat
@@ -1,13 +1,25 @@
 @echo off
 echo=
 
-echo "请先安装好dev C++工具minGW"
+:: 1. minGW，有bug，打包后无法执行，等研究合适了再修复
 
-echo windresing...
-windres -o md5.exe.syso md5.exe.rc
+:: echo "请先安装好dev C++工具minGW"
+
+:: echo windresing...
+:: windres -o md5.exe.syso md5.exe.rc
+
+:: echo building...
+:: go build -ldflags="-H windowsgui -w -s"
+
+:: 2.rsrc
+
+echo "需要先安装工具命令rsrc，命令：go get github.com/akavel/rsrc" 
+
+echo rsrc manifesting...
+rsrc -manifest md5.exe.manifest -o md5.exe.syso -ico md5.ico
 
 echo building...
-go build -ldflags="-H windowsgui -w -s"
+go build
 
 echo=
 pause
