07546653d9cc931d008f3c4f262aa457432eb68a
Anttu <78537438@qq.com>
2021-02-26 15:56:36 +0800

    modify

--- utiltool/toy/pic2ascii.go
+++ utiltool/toy/pic2ascii.go
@@ -5,6 +5,7 @@ import (
 	"github.com/qeesung/image2ascii/convert"
 	_ "image/jpeg"
 	_ "image/png"
+	"os"
 )
 
 func main() {
@@ -12,10 +13,24 @@ func main() {
 
 	var o convert.Options
 	o.Ratio = 0.3
-	o.Colored = true
+	//o.Colored = true
 
 	converter := convert.NewImageConverter()
-	fmt.Print(converter.ImageFile2ASCIIString(path, &o))
 
-	// 目前输出到控制填，下一步输出到txt，彩色需要设计下
+	// 目前输出到控制台
+	//fmt.Print(converter.ImageFile2ASCIIString(path, &o))
+
+	// 输出到txt，彩色需要研究下。输出txt暂时不支持彩色了，彩色需要用html
+	str := converter.ImageFile2ASCIIString(path, &o)
+
+	fileName := "test.txt"
+	dstFile, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer dstFile.Close()
+
+	dstFile.WriteString(str)
 }
